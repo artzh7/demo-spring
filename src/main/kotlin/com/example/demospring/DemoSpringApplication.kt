2f4dd0741360c741c0f92a5b0666f61714f3c214
artzh <art.zh@inbox.ru>
2023-02-22 12:18:36 +0300

    cache expire after write

--- src/main/kotlin/com/example/demospring/DemoSpringApplication.kt
+++ src/main/kotlin/com/example/demospring/DemoSpringApplication.kt
@@ -2,10 +2,8 @@ package com.example.demospring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
-@EnableCaching
 class DemoSpringApplication
 
 fun main(args: Array<String>) {
